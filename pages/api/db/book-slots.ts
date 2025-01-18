f7eb0ce029d43d69e266d0ffd200be45514b8f31
Tamil0105 <tamil16399@gmail.com>
2025-01-18 08:51:12 +0530

    BUILD CHECK

--- pages/api/db/book-slots.ts
+++ pages/api/db/book-slots.ts
@@ -97,7 +97,6 @@ export default async function handler(
       // Convert grouped data object to an array
       const result = Object.values(groupedData);
 
-      console.log(result)
 
       res.status(200).json({
         bookings: result,
